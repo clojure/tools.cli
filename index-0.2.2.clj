66784f04aa7daa1eea9d0cbedf7fb04085333d18
Tom Faulhaber <git_net@infolace.com>
2011-12-02 17:44:36 -0800

    Autodoc commit for master/a73ac78a

---
+++ index-0.2.2.clj
@@ -0,0 +1,9 @@
+{:namespaces
+ ({:source-url
+   "https://github.com/clojure/tools.cli/blob/47984bc7413d7a675ea9586d08d461d4a016060e/src/main/clojure/clojure/tools/cli.clj",
+   :wiki-url
+   "http://clojure.github.com/tools.cli/clojure.tools.cli-api.html",
+   :name "clojure.tools.cli",
+   :author "Gareth Jones",
+   :doc nil}),
+ :vars ()}

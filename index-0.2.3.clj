173b936abb75ec89561769a9c0ba2ec41d4a074c
Tom Faulhaber <git_net@infolace.com>
2012-11-14 22:07:40 -0800

    Autodoc commit for master/94c9483f

--- index-0.2.3.clj
+++ index-0.2.3.clj
@@ -1,6 +1,6 @@
 {:namespaces
  ({:source-url
-   "https://github.com/clojure/tools.cli/blob/ec7bdb3b1189db0dfcd7b1c0d92e784eef84f926/src/main/clojure/clojure/tools/cli.clj",
+   "https://github.com/clojure/tools.cli/blob/f9d92395cb788dd08cb144035a9d5fd8706d10b5/src/main/clojure/clojure/tools/cli.clj",
    :wiki-url
    "http://clojure.github.com/tools.cli/clojure.tools.cli-api.html",
    :name "clojure.tools.cli",
@@ -11,13 +11,13 @@
    :name "cli",
    :namespace "clojure.tools.cli",
    :source-url
-   "https://github.com/clojure/tools.cli/blob/ec7bdb3b1189db0dfcd7b1c0d92e784eef84f926/src/main/clojure/clojure/tools/cli.clj#L108",
+   "https://github.com/clojure/tools.cli/blob/f9d92395cb788dd08cb144035a9d5fd8706d10b5/src/main/clojure/clojure/tools/cli.clj#L112",
    :raw-source-url
-   "https://github.com/clojure/tools.cli/raw/ec7bdb3b1189db0dfcd7b1c0d92e784eef84f926/src/main/clojure/clojure/tools/cli.clj",
+   "https://github.com/clojure/tools.cli/raw/f9d92395cb788dd08cb144035a9d5fd8706d10b5/src/main/clojure/clojure/tools/cli.clj",
    :wiki-url
    "http://clojure.github.com/tools.cli//clojure.tools.cli-api.html#clojure.tools.cli/cli",
    :doc
    "Parse the provided args using the given specs. Specs are vectors\ndescribing a command line argument. For example:\n\n[\"-p\" \"--port\" \"Port to listen on\" :default 3000 :parse-fn #(Integer/parseInt %)]\n\nFirst provide the switches (from least to most specific), then a doc\nstring, and pairs of options.\n\nValid options are :default, :parse-fn, and :flag. See\nhttps://github.com/clojure/tools.cli/blob/master/README.md for more\ndetailed examples.\n\nReturns a vector containing a map of the parsed arguments, a vector\nof extra arguments that did not match known switches, and a\ndocumentation banner to provide usage instructions.",
    :var-type "function",
-   :line 108,
+   :line 112,
    :file "src/main/clojure/clojure/tools/cli.clj"})}

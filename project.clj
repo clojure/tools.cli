e1838cb25c55c5072654e2495a9f557e768fcf8a
guns <self@sungpae.com>
2014-04-21 22:25:24 -0500

    Update dev project dependencies

--- project.clj
+++ project.clj
@@ -7,12 +7,12 @@
   :source-paths ["src/main/clojure"]
   :test-paths ["src/test/clojure"]
   :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
-  :dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]]
   :profiles {:1.2 {:dependencies [[org.clojure/clojure "1.2.1"]]}
              :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :1.6 {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
 
              ;; Local CLJS development; not in pom.xml
              :dev {:dependencies [[org.clojure/clojurescript "0.0-2080"]]
